d556e52fcf7505f05c97c62b76d7720c7d596e6e
hyuseinleshov <hyusein.leshov@gmail.com>
2025-01-16 17:16:42 +0200

    feat(typescript-training): create loops and arrays - write some code - part 2

---
+++ typescript-training/03-loops-and-arrays/growable-arrays.js
@@ -0,0 +1,6 @@
+var sportsTwo = ["Golf", "Cricket", "Tennis"];
+sportsTwo.push("Baseball");
+sportsTwo.push("Football");
+for (var i = 0; i < sportsTwo.length; i++) {
+    console.log(sportsTwo[i]);
+}
---
+++ typescript-training/03-loops-and-arrays/growable-arrays.ts
@@ -0,0 +1,8 @@
+let sportsTwo: string[] = ["Golf", "Cricket", "Tennis"];
+
+sportsTwo.push("Baseball");
+sportsTwo.push("Football");
+
+for (let i = 0; i < sportsTwo.length; i++) {
+    console.log(sportsTwo[i]);
+}
