11b7ed50a98e0e370cbc2f93ef7d8452bcdad2fd
hyuseinleshov <hyusein.leshov@gmail.com>
2025-01-29 20:18:12 +0200

    feat(react-library): home page - explore top books component
    Reformat code.

--- library-app/03-frontend/react-library/src/App.tsx
+++ library-app/03-frontend/react-library/src/App.tsx
@@ -1,10 +1,14 @@
-import './App.css'
-import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import "./App.css";
+import { ExploreTopBooks } from "./layouts/HomePage/ExploreTopBooks";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 
 function App() {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <ExploreTopBooks />
+    </div>
   );
 }
 
-export default App
+export default App;
