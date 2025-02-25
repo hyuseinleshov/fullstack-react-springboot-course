75eaa928003d12118b873524f05c90929cc24f74
hyuseinleshov <hyusein.leshov@gmail.com>
2025-02-25 17:51:12 +0200

    feat(react-library): add pagination to search books page component - part 2

--- library-app/03-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ library-app/03-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -56,6 +56,7 @@ export const SearchBooksPage = () => {
       setIsLoading(false);
       setHttpError(error.message);
     });
+    window.scrollTo(0, 0);
   }, [currentPage, booksPerPage]);
 
   if (isLoading) {
@@ -140,9 +141,11 @@ export const SearchBooksPage = () => {
             </div>
           </div>
           <div className="mt-3">
-            <h5>Number of results: (22)</h5>
+            <h5>Number of results: ({totalAmountOfBooks})</h5>
           </div>
-          <p>1 to 5 of 22 items:</p>
+          <p>
+            {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
+          </p>
           {books.map((book) => (
             <SearchBook book={book} key={book.id} />
           ))}
