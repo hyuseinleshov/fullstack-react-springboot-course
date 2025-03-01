d75c7b092be42f8b2a601feafe1e03b2d537974d
hyuseinleshov <hyusein.leshov@gmail.com>
2025-03-01 17:58:13 +0200

    feat(react-library): checkout link

--- library-app/03-frontend/react-library/src/layouts/SearchBooksPage/components/SearchBook.tsx
+++ library-app/03-frontend/react-library/src/layouts/SearchBooksPage/components/SearchBook.tsx
@@ -1,5 +1,6 @@
 import BookModel from "../../../models/BookModel";
 import bookImage from "../../../Images/BooksImages/book-luv2code-1000.png";
+import { Link } from "react-router-dom";
 
 export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
   return (
@@ -32,9 +33,9 @@ export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
           </div>
         </div>
         <div className="col-md-4 d-flex justify-content-center align-items-center">
-          <a className="btn btn-md main-color text-white" href="#">
+          <Link className="btn btn-md main-color text-white" to={`/checkout/${props.book.id}`}>
             View Details
-          </a>
+          </Link>
         </div>
       </div>
     </div>
