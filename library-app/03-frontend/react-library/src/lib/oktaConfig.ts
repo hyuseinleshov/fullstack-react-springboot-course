804a6f07ca31f32da9cdc93488c40a4027b634cd
hyuseinleshov <hyusein.leshov@gmail.com>
2025-03-03 13:41:32 +0200

    feat(react-library): develop app configuration

---
+++ library-app/03-frontend/react-library/src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: import.meta.env.VITE_OKTA_CLIENT_ID,
+    issuer: import.meta.env.VITE_OKTA_ISSUER,
+    redirectUri: import.meta.env.VITE_OKTA_REDIRECT_URI,
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true,
+};
