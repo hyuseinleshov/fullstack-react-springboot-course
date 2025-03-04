02d567fadf275241dae4e0aa8c8f13f62c93fb40
hyuseinleshov <hyusein.leshov@gmail.com>
2025-03-04 17:00:40 +0200

    feat(spring-boot-library): checkout repository

---
+++ library-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/CheckoutRepository.java
@@ -0,0 +1,10 @@
+package com.luv2code.springbootlibrary.dao;
+
+import com.luv2code.springbootlibrary.entity.Checkout;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
+    Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+}
