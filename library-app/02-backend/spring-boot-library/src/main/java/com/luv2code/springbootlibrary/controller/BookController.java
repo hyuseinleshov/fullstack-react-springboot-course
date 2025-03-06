bbdf8f60ffe96692f57c594404fc334489b4d8c0
hyuseinleshov <hyusein.leshov@gmail.com>
2025-03-06 22:09:05 +0200

    feat(spring-boot-library): book controller enhancement

---
+++ library-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.luv2code.springbootlibrary.controller;
+
+import com.luv2code.springbootlibrary.entity.Book;
+import com.luv2code.springbootlibrary.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:5173")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+        String userEmail = "testuser@email.com";
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+}
