35f0cd2f351faa4b25bd545b665eb60b81ffdba3
hyuseinleshov <hyusein.leshov@gmail.com>
2025-03-06 22:21:23 +0200

    feat(spring-boot-library): current loans count

--- library-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/CheckoutRepository.java
+++ library-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/CheckoutRepository.java
@@ -4,7 +4,11 @@ import com.luv2code.springbootlibrary.entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
     Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+
+    List<Checkout> findBooksByUserEmail(String userEmail);
 }
