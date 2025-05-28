ca76b1fa59709114434bc7725659a5e6f92728c2
hyuseinleshov <hyusein.leshov@gmail.com>
2025-05-28 16:17:40 +0300

    feat(react-library): submit review

--- library-app/03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ library-app/03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -7,6 +7,7 @@ import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
 import ReviewModel from "../../models/ReviewModel";
 import { LatestReviews } from "./LatestReviews";
 import { useOktaAuth } from "@okta/okta-react";
+import { request } from "http";
 
 export const BookCheckoutPage = () => {
   const { authState } = useOktaAuth();
@@ -226,6 +227,33 @@ export const BookCheckoutPage = () => {
       throw new Error("Something went wrong!");
     }
     setIsCheckedOut(true);
+  }
+
+  async function submitReview(starInput: number, reviewDescription: string) {
+    let bookId: number = 0;
+    if (book?.id) {
+      bookId = book.id;
+    }
+
+    const reviewRequestModel = new ReviewRequestModel(
+      starInput,
+      bookId,
+      reviewDescription
+    );
+    const url = "http://localhost:8080/api/reviews/secure";
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reviewRequestModel),
+    };
+    const returnResponse = await fetch(url, requestOptions);
+    if (!returnResponse.ok) {
+      throw new Error("Something went wrong!");
+    }
+    setIsReviewLeft(true);
   }
 
   return (
