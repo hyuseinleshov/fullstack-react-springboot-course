484665e62675834c5913a514278c4f05f04c41b1
hyuseinleshov <hyusein.leshov@gmail.com>
2025-05-28 15:51:28 +0300

    feat(react-library): review render

--- library-app/03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ library-app/03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -254,6 +254,7 @@ export const BookCheckoutPage = () => {
             isAuthenticated={authState?.isAuthenticated}
             isCheckedOut={isCheckedOut}
             checkoutBook={checkoutBook}
+            isReviewLeft={isReviewLeft}
           />
         </div>
         <hr />
@@ -282,6 +283,7 @@ export const BookCheckoutPage = () => {
           isAuthenticated={authState?.isAuthenticated}
           isCheckedOut={isCheckedOut}
           checkoutBook={checkoutBook}
+          isReviewLeft={isReviewLeft}
         />
         <hr />
         <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
