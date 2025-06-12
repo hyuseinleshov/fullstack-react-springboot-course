61c01be9976f38e7e1a17086f0d0cf45a0d66ea6
hyuseinleshov <hyusein.leshov@gmail.com>
2025-06-12 09:43:57 +0300

    Spring Boot - Message Service

---
+++ library-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/MessageService.java
@@ -0,0 +1,26 @@
+package com.luv2code.springbootlibrary.service;
+
+import com.luv2code.springbootlibrary.dao.MessageRepository;
+import com.luv2code.springbootlibrary.entity.Message;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class MessageService {
+
+    private MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public void postMessage(Message messageRequest, String userEmail) {
+        Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
+        message.setUserEmail(userEmail);
+        messageRepository.save(message);
+    }
+
+}
