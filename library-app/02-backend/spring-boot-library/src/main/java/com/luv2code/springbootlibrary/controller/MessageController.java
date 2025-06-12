e7a179a7c22873d0e4a86d568b79472750d26c22
hyuseinleshov <hyusein.leshov@gmail.com>
2025-06-12 09:47:58 +0300

    Spring Boot - Message Controller

---
+++ library-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/MessageController.java
@@ -0,0 +1,28 @@
+package com.luv2code.springbootlibrary.controller;
+
+import com.luv2code.springbootlibrary.entity.Message;
+import com.luv2code.springbootlibrary.service.MessageService;
+import com.luv2code.springbootlibrary.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:5173")
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+
+    private MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/secure/add/message")
+    public void postMessage(@RequestHeader(value = "Authorization") String token,
+                            @RequestBody Message messageRequest) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        messageService.postMessage(messageRequest, userEmail);
+    }
+
+}
