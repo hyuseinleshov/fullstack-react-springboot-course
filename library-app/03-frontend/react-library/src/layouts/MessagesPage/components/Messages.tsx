43ae5be6cab549353da49da0539a80b6c6b53dfe
hyuseinleshov <hyusein.leshov@gmail.com>
2025-06-13 16:21:02 +0300

    React - Message Component HTML/CSS

--- library-app/03-frontend/react-library/src/layouts/MessagesPage/components/Messages.tsx
+++ library-app/03-frontend/react-library/src/layouts/MessagesPage/components/Messages.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useOktaAuth } from "@okta/okta-react";
 import MessageModel from "../../../models/MessageModel";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+import { Pagination } from "../../Utils/Pagination";
 
 export const Messages = () => {
   const { authState } = useOktaAuth();
@@ -19,7 +20,7 @@ export const Messages = () => {
   useEffect(() => {
     const fetchUserMessages = async () => {
       if (authState && authState?.isAuthenticated) {
-        const url = `http://localhost:8080/api/messages/search/findByUserEmail/?userEmail=${
+        const url = `http://localhost:8080/api/messages/search/findByUserEmail?userEmail=${
           authState?.accessToken?.claims.sub
         }&page=${currentPage - 1}&size=${messagesPerPage}`;
         const requestOptions = {
@@ -60,5 +61,49 @@ export const Messages = () => {
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
-  return "";
+  return (
+    <div className="mt-2">
+      {messages.length > 0 ? (
+        <>
+          <h5>Current Q/A: </h5>
+          {messages.map((message) => (
+            <div key={message.id}>
+              <div className="card mt-2 shadow p-3 bg-body rounded">
+                <h5>
+                  Case #{message.id}: {message.title}
+                </h5>
+                <h6>{message.userEmail}</h6>
+                <p>{message.question}</p>
+                <hr />
+                <div>
+                  <h5>Response: </h5>
+                  {message.response && message.adminEmail ? (
+                    <>
+                      <h6>{message.adminEmail} (admin)</h6>
+                      <p>{message.response}</p>
+                    </>
+                  ) : (
+                    <p>
+                      <i>
+                        Pending response from administration. Please be patient.
+                      </i>
+                    </p>
+                  )}
+                </div>
+              </div>
+            </div>
+          ))}
+        </>
+      ) : (
+        <h5>All questions you submit will be shown here</h5>
+      )}
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          paginate={paginate}
+        />
+      )}
+    </div>
+  );
 };
