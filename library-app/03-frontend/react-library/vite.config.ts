c6f91c972f181c30d76b0e780eb71dddbfff49d5
hyuseinleshov <hyusein.leshov@gmail.com>
2025-06-23 11:03:57 +0300

    React - HTTPS config for Vite

--- library-app/03-frontend/react-library/vite.config.ts
+++ library-app/03-frontend/react-library/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync("ssl-localhost/localhost.key"),
+      cert: fs.readFileSync("ssl-localhost/localhost.crt"),
+    },
+    port: 5173,
+  },
+});
