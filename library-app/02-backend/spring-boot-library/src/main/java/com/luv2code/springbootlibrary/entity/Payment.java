6c65c3631b030700ee2eac6da8a157651667c9e9
hyuseinleshov <hyusein.leshov@gmail.com>
2025-06-26 09:19:02 +0300

    Spring Boot - Create Payment Entity

---
+++ library-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/entity/Payment.java
@@ -0,0 +1,41 @@
+package com.luv2code.springbootlibrary.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    private double amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
