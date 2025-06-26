1270b3d5c8a2268a738b9d28f33eea2c155426cd
hyuseinleshov <hyusein.leshov@gmail.com>
2025-06-26 12:23:59 +0300

    React - Index Configurations

--- library-app/03-frontend/react-library/src/main.tsx
+++ library-app/03-frontend/react-library/src/main.tsx
@@ -3,11 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51RdpQKDEJkquwNe8AgKKpNBVDWpNF2zUUUEcebJcaFjNcpNmP9EQf250ATDKMdFmV68UmoXXh11SG92ocJ8WOtor00zAldihpT"
+);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Elements stripe={stripePromise}>
+        <App />
+      </Elements>
     </BrowserRouter>
   </StrictMode>
 );
